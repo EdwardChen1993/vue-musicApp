3f3468a3155fa5115c981f21192d573f8cfd2258
chenzehua <872990547@qq.com>
2017-07-26 17:35:38 +0800

    第四章推荐页面开发完成

--- src/main.js
+++ src/main.js
@@ -1,16 +1,25 @@
+import 'babel-polyfill'
 import Vue from 'vue'
 import App from './App'
-// import router from './router'
+import router from './router'
+import fastclick from 'fastclick'
+import vueLazyLoad from 'vue-lazyload'
 
 import 'common/stylus/index.styl'
+
+fastclick.attach(document.body)
+
+Vue.use(vueLazyLoad,{
+  loading: require('common/image/default.png')
+})
 
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
-  render: h => h(App)
-  // router,
+  router,
+  render: h => h(App),
   // template: '<App/>',
   // components: { App }
 })
