b87e2b564cd8fb0fb027ef05c4cfcd47b76b9b8c
chenzehua <872990547@qq.com>
2017-07-27 16:57:32 +0800

    第五章歌手页面开发完成

--- src/base/scroll/scroll.vue
+++ src/base/scroll/scroll.vue
@@ -23,6 +23,10 @@
         default: function () {
           return null;
         }
+      },
+      listenScroll: {
+        type: Boolean,
+        default: false
       }
     },
     mounted() {
@@ -39,6 +43,12 @@
           probeType: this.probeType,
           click: this.click
         })
+        if (this.listenScroll) {
+          let self = this;
+          this.scroll.on('scroll', (pos) => {
+            self.$emit('scroll', pos)
+          })
+        }
       },
       enable() {
         this.scroll && this.scroll.enable();
@@ -48,6 +58,12 @@
       },
       refresh() {
         this.scroll && this.scroll.refresh();
+      },
+      scrollTo() {
+        this.scroll && this.scroll.scrollTo.apply(this.scroll, arguments)
+      },
+      scrollToElement() {
+        this.scroll && this.scroll.scrollToElement.apply(this.scroll, arguments);
       }
     },
     watch: {
--- src/common/js/dom.js
+++ src/common/js/dom.js
@@ -1,5 +1,5 @@
-export function addClass(el,className) {
-  if (hasClass(el,className)) {
+export function addClass(el, className) {
+  if (hasClass(el, className)) {
     return;
   }
   let newClass = el.className.split(' ');
@@ -7,7 +7,18 @@ export function addClass(el,className) {
   el.className = newClass.join(' ');
 }
 
-export function hasClass(el,className) {
-  let reg = new RegExp('(^|\\s)'+className+'(\\s|$)')
+export function hasClass(el, className) {
+  let reg = new RegExp('(^|\\s)' + className + '(\\s|$)')
   return reg.test(el.className);
+}
+
+export function getData(el, name, val) {
+  const prefix = 'data-';
+  name = prefix + name;
+  if (val) {
+    return el.setAttribute(name, val);
+  }
+  else {
+    return el.getAttribute(name);
+  }
 }
